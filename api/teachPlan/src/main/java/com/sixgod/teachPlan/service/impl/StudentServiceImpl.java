f2111ea1f0b0cc4c8674949b642dff07003a9863
moujie1998 <1659411502@qq.com>
2019-11-21 21:25:42 +0800

    getLoginStudent

--- api/teachPlan/src/main/java/com/sixgod/teachPlan/service/impl/StudentServiceImpl.java
+++ api/teachPlan/src/main/java/com/sixgod/teachPlan/service/impl/StudentServiceImpl.java
@@ -4,6 +4,7 @@ import com.sixgod.teachPlan.entity.Student;
 import com.sixgod.teachPlan.entity.User;
 import com.sixgod.teachPlan.repository.StudentRepository;
 import com.sixgod.teachPlan.service.StudentService;
+import com.sixgod.teachPlan.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,13 @@ import javax.jws.soap.SOAPBinding;
 
 @Service
 public class StudentServiceImpl implements StudentService {
-    @Autowired StudentService studentService;
+    @Autowired
+    UserService userService;
     @Autowired
     StudentRepository studentRepository;
     @Override
     public Student getCurrentLoginStudent() {
-        Student student = studentService.getCurrentLoginStudent();
-        return studentRepository.findByUserId(student.getId());
+        User user = userService.getCurrentLoginUser();
+        return studentRepository.findByUserId(user.getId());
     }
 }
